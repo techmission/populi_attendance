83a371b2e929ce68afe38f9e1d352c81a124a026
Evan Donovan <evan@techmission.org>
2018-07-13 18:57:47 +0100

    Added access control and modified error handling

--- attendance/main.php
+++ attendance/main.php
@@ -5,19 +5,23 @@
 chdir(dirname(__FILE__));
 
 require_once('controller.inc'); // add main business logic
-bootstrap();                    // add rest of code
-
-// Build the attendance data array; uses data from $_GET
-$attendance_data = build_attendance_data();
+bootstrap(); // add rest of code
 
 // Render using Twig.
-// See https://twig.sensiolabs.org/doc/2.x/api.html#twig-for-developers
+// See https://twig.sensiolabs.org/doc/1.x/api.html#twig-for-developers
 global $TWIG;
 // Debugging vars
 global $VARS;
 
-$template_file = 'attendance.html';
+// Access control: if not using the GUID, then render an access denied template.
+if(!isset($_GET['key']) && $_GET['key'] != GUID) {
+  $template = $TWIG->load('access_denied.html');
+  echo $template->render(array('vars' => $VARS));
+}
+
+// Build the attendance data array; uses data from $_GET
+$attendance_data = build_attendance_data();
 
 $template = $TWIG->load(TEMPLATE_FILE);
-echo $template->render(array('a' => $attendance_data, 'vars' => $VARS));
+echo $template->render(array('a' => $attendance_data, 'vars' => $VARS, 'a_pre' => print_r($attendance_data, TRUE)));
 ?>
